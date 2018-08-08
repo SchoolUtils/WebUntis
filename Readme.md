8ddee6c27a4b28673e7270992e88523d3dab51a0
Nils <magiccraft.nitrado@gmail.com>
2018-08-08 12:13:15 +0200

    Fix Docs

--- Readme.md
+++ Readme.md
@@ -2,4 +2,4 @@
 
 This is a NodeJS Wrapper for the JSON RPC WebUntis API.
 
-The Documentation is available at [https://noim.me/WebUntis/latest/](https://noim.me/WebUntis/latest/)
+The Documentation is available at [https://noim.me/WebUntis/](https://noim.me/WebUntis/)
--- linkDocs.js
+++ linkDocs.js
@@ -4,17 +4,8 @@ const symlinkDir = require('symlink-dir')
 const fs = require("fs-extra");
 
 if (fs.pathExistsSync(path.join(__dirname, "./docs/latest"))) fs.removeSync(path.join(__dirname, "./docs/latest"));
-
-fs.ensureDirSync(path.join(__dirname, "./docs/latest"));
-fs.ensureFileSync(path.join(__dirname, "./docs/latest/index.html"));
-fs.outputFileSync(path.join(__dirname, "./docs/latest/index.html"), `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Redirect</title>
-    <meta http-equiv="refresh" content="URL=https://noim.me/WebUntis/webuntis/${version}/">
-</head>
-<body>
-
-</body>
-</html>`);
+symlinkDir(path.join(__dirname, `./docs/webuntis/${version}/`), path.join(__dirname, "./docs/latest")).then(() => {
+    process.exit(0);
+}).catch(e => {
+    process.exit(1);
+})
