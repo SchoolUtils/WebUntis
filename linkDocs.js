ad5bbb70551b404de89b324565a9eb8dc8ff6a7c
Nils <magiccraft.nitrado@gmail.com>
2018-08-08 12:15:42 +0200

    Fix Docs

--- linkDocs.js
+++ linkDocs.js
@@ -3,8 +3,8 @@ const path = require("path");
 const symlinkDir = require('symlink-dir')
 const fs = require("fs-extra");
 
-if (fs.pathExistsSync(path.join(__dirname, "./docs/latest"))) fs.removeSync(path.join(__dirname, "./docs/latest"));
-symlinkDir(path.join(__dirname, `./docs/webuntis/${version}/`), path.join(__dirname, "./docs/latest")).then(() => {
+if (fs.pathExistsSync(path.join(__dirname, "./docs/"))) fs.removeSync(path.join(__dirname, "./docs/"));
+symlinkDir(path.join(__dirname, `./doc/webuntis/${version}/`), path.join(__dirname, "./docs/")).then(() => {
     process.exit(0);
 }).catch(e => {
     process.exit(1);
